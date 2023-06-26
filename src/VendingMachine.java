3f8ec72ebf2f6af560c7870d2b31fecdabc7ce2e
Renz Wendell C. Abino <renz_abino@dlsu.edu.ph>
2023-06-26 13:56:14 +0800

    Changed addQuantity to setQuantity Changed printInventory to regular getter of getInventory Added Sale class (Although this one isn't finalized yet, I realized we need to finish first the denominations class and the methods for vendingmachine) Added Denomination class

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,33 +1,40 @@
-import java.util.ArrayList;
+import model.Item;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class VendingMachine {
     private Map<Item, Integer> inventory = new HashMap<>();
 
-    public void addItem(Item item, int quantity) {
+
+    // The moderator can use this for versatility.
+    public void setItem(Item item, int quantity) { // TODO revise the addItem, because this method performs setItemQuantity
+
         if (quantity <= 20){
             System.out.printf("\tAdding item %s with quantity %d...\n", item.getName(), quantity);
             inventory.put(item, quantity);
         }
-//        else if{
-//            System.out.println("\tThere is no more space for " + item.getName() + ".");
-//        }
-    }
-    public void addItem(Item item) {
-        this.addItem(item, 10);
-    }
-
-    public void printInventory(){
-        ArrayList<Item> items = new ArrayList<>( inventory.keySet() );
-        ArrayList<Integer> quantities = new ArrayList<>( inventory.values() );
-        System.out.println("\nInventory:");
-        for (int i = 0; i < inventory.size(); i++){
-            Item oneItem = items.get(i);
-            int oneQuantity = quantities.get(i);
-
-            System.out.printf("%d. %s x%d: \n\tPrice per item: %s\n", i+1, oneItem.getName(), oneQuantity, oneItem.getPrice());
+        else{
+            System.out.println("\tThere is no more space for " + item.getName() + ".");
         }
     }
 
+    // The moderator can use this for convenience to ensure that the minimum criteria for the quantity is met.
+    public void setItem(Item item) { // TODO revise the addItem, because this method performs setItemQuantity
+        this.setItem(item, 10);
+    }
+
+    public Map<Item, Integer> getInventory(){ // TODO this should not print, retrieve (not printInventory but getInventory)
+        return inventory;
+//        ArrayList<model.Item> items = new ArrayList<>( inventory.keySet() );
+//        ArrayList<Integer> quantities = new ArrayList<>( inventory.values() );
+//        System.out.println("\nInventory:");
+//        for (int i = 0; i < inventory.size(); i++){
+//            model.Item oneItem = items.get(i);
+//            int oneQuantity = quantities.get(i);
+//
+//            System.out.printf("%d. %s x%d: \n\tPrice per item: %s\n", i+1, oneItem.getName(), oneQuantity, oneItem.getPrice());
+        }
 }
+
+
--- src/model/Item.java
+++ src/model/Item.java
@@ -1,14 +1,23 @@
+package model;
+
+import java.util.ArrayList;
+
 public class Item {
     private String name;
     private double price;
+    private double calories;
+    private ArrayList<Sale> sales;
 
-    public Item(String name, double price) {
+    public Item(String name, double price, double calories) {
         this.name = name;
         this.price = price;
+        this.calories = calories;
     }
 
     public String getName(){return name;}
     public double getPrice(){return price;}
+    public double getCalories() { return calories; }
+    public ArrayList<Sale> getSales() { return sales; }
 
     // Setter for price to allow changes in pricing
     public void setPrice(double price) {
